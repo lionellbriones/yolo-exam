011b7be0320cb37866a46d7f980a921489cb7b16
Lionell Briones <llenoil@gmail.com>
2019-05-17 21:33:01 +0800

    Configure store on App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Provider } from "react-redux";
 import "./App.css";
 
 import Header from "./shared/components/Header";
 import Search from "./shared/components/Search";
 import JobListing from "./shared/components/JobListing";
 
+import configureStore from "./store";
+const store = configureStore();
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-Wrapper">
-          <Header />
-          <Search />
-          <JobListing />
+          <Provider store={store}>
+            <Fragment>
+              <Header />
+              <Search />
+              <JobListing />
+            </Fragment>
+          </Provider>
         </div>
       </div>
     );
