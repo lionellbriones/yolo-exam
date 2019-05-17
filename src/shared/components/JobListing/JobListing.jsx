e4f3becb771c3fcaa032b2912154ae1b9822250f
Lionell Briones <llenoil@gmail.com>
2019-05-17 23:35:52 +0800

    Added initial fetch and display

--- src/shared/components/JobListing/JobListing.jsx
+++ src/shared/components/JobListing/JobListing.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./JobListing.css";
 
-const JobListing = () => <div>Job Listing Section</div>;
+class JobListing extends Component {
+  state = {
+    jobs: []
+  };
+
+  componentDidMount() {
+    fetch(
+      `https://search.bossjob.com/api/v1/search/job_filter?size=10&query=system`
+    )
+      .then(res => res.json())
+      .then(jobList => {
+        this.setState({
+          jobs: jobList.data.jobs
+        });
+      });
+  }
+
+  render() {
+    const { jobs } = this.state;
+    return (
+      <div className="JobListingContainer">
+        <section className="JobCardContainer">
+          {jobs.map(job => (
+            <div className="JobCard" key={job.id}>
+              <h3>{job.job_category}</h3>
+              <p>{job.job_location}</p>
+              <p>{job.xp_lvl}</p>
+              <p>{job.degree}</p>
+              <p>{job.job_type}</p>
+              <img src={job.company_logo} style={{ width: "100px" }} />
+              <p>{job.updated_at}</p>
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
+}
 
 export default JobListing;
