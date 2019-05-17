b1fb74779bf82ee639b204a086b113a9aa1002f0
Lionell Briones <llenoil@gmail.com>
2019-05-17 21:25:48 +0800

    Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+// Constants to be used for Saga actions and effect
+const JOBS = {
+  LOAD: "JOBS_LOAD",
+  LOAD_SUCCESS: "JOBS_LOAD_SUCCESS",
+  LOAD_FAIL: "JOBS_LOAD_FAIL"
+};
+
+export { JOBS };
