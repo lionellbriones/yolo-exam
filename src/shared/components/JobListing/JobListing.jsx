3e74f6a0d72c180ba5823b49e4b35870cefb3ccb
Lionell Briones <llenoil@gmail.com>
2019-05-18 13:03:25 +0800

    Added style for job listing

--- src/shared/components/JobListing/JobListing.jsx
+++ src/shared/components/JobListing/JobListing.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import moment from "moment";
+
 import { connect } from "react-redux";
 import { loadJobs } from "../../../actions";
 
@@ -13,20 +15,35 @@ class JobListing extends Component {
     const { jobs } = this.props;
     return (
       <div className="JobListingContainer">
-        <section className="JobCardContainer">
+        <section>
           {jobs.map(job => (
             <div className="JobCard" key={job.id}>
-              <h3>{job.job_category}</h3>
-              <p>{job.job_location}</p>
-              <p>{job.xp_lvl}</p>
-              <p>{job.degree}</p>
-              <p>{job.job_type}</p>
-              <img
-                src={job.company_logo}
-                alt={job.company_name}
-                style={{ width: "100px" }}
-              />
-              <p>{job.updated_at}</p>
+              <div className="JobCardTitleContainer">
+                <h3 className="JobCardTitle">{job.job_category}</h3>
+              </div>
+              <div className="JobCardDetails">
+                <div className="JobCardDetailsGroup">
+                  <span className="JobCardDetailsItem">{job.job_location}</span>
+                  <span className="JobCardDetailsItem">{job.xp_lvl}</span>
+                </div>
+                <div className="JobCardDetailsGroup">
+                  <span className="JobCardDetailsItem">{job.degree}</span>
+                  <span className="JobCardDetailsItem">{job.job_type}</span>
+                </div>
+              </div>
+              <div className="JobCardCompany">
+                <div className="JobCardCompanyDetails">
+                  <img
+                    className="JobCardCompanyLogo"
+                    src={job.company_logo}
+                    alt={job.company_name}
+                  />
+                  <span>{job.company_name}</span>
+                </div>
+                <div className="JobCardCompanyMoment">
+                  <span>{moment(job.created_at).fromNow()}</span>
+                </div>
+              </div>
             </div>
           ))}
         </section>
